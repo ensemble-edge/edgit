674bab1b5d8ed58da16c40f55381e135d0b8b724
maycotte <ho@maycotte.com>
2025-10-30 23:06:21 +0000

    Test name change detection

--- test-name-change.js
+++ test-name-change.js
@@ -1,9 +1,12 @@
+/**
+ * Edgit: id=tkkm7t version=1.0.1 component=awesome-new-name
+ */
 #!/usr/bin/env node
 
 // Test script to demonstrate the name change workflow
-// Edgit: id=tkkm7t version=1.0.0 component=test-name-change-agent
+// Edgit: id=tkkm7t version=1.0.0 component=awesome-new-name
 
-console.log('This is a test component with the original name');
+console.log('This is a test component with a MANUALLY CHANGED name!');
 
 function greet(name) {
   return `Hello, ${name}!`;
