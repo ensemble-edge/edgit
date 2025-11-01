f7d1187fff27970a94dda3bdf250b9825b699530
maycotte <ho@maycotte.com>
2025-11-01 21:29:26 +0000

    build: update compiled dist files
    Include dist/ build artifacts from unit test additions

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+/**
+ * Edgit - Git-native component versioning
+ * Copyright (c) 2024-2025 Higher Order Capital
+ * Licensed under the MIT License
+ *
+ * Ensemble® is a registered trademark of Higinio O. Maycotte.
+ */
 // Load environment variables first
 import dotenv from 'dotenv';
 dotenv.config();
