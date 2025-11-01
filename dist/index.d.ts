6f16068783afb1318291c23adb25b9b3c5e1bae6
maycotte <ho@maycotte.com>
2025-11-01 21:29:26 +0000

    build: update compiled dist files
    Include dist/ build artifacts from unit test additions

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+/**
+ * Edgit - Git-native component versioning
+ * Copyright (c) 2024-2025 Higher Order Capital
+ * Licensed under the MIT License
+ *
+ * Ensemble® is a registered trademark of Higinio O. Maycotte.
+ */
 export {};
 //# sourceMappingURL=index.d.ts.map
